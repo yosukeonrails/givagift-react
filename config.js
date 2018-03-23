aeb1482066a625c29562244f3fcd936527febc63
yosukeyoh <yosukeonrails@gmail.com>
2018-03-23 15:44:51 -0700

    replaced npm with yarn

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 
 exports.URL= process.env.URL || 'http://localhost:8080/';
 
-exports.clientID= process.env.clientID || '1461195987233806';
+exports.clientID= process.env.CLIENT_ID || '1461195987233806';
 
-exports.clientSecret= process.env.clientSecret || '93f6c0e5811b441be57777a239001801';
+exports.clientSecret= process.env.CLIENT_SECRET || '93f6c0e5811b441be57777a239001801';
 
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
